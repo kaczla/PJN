d8fe7f2424ec933cd72fd90e288113a325ca2892
kaczla <kaczla@wp.pl>
2015-10-20 13:01:14 +0200

    Update

--- src/2015.10.15/Task205Test.sh
+++ src/2015.10.15/Task205Test.sh
@@ -2,7 +2,9 @@
 
 # Zadanie 205
 
+# Napisz program Task205.py, który wypisze wszystkie takie zbiory wyrazów (jedna linia to jeden zbiór, wyrazy oddzielone spacją, kolejność nieistotna), w których wyrazy po usunięciu znaków diakrytycznych reprezentują to samo słowo (np. bąka, baka, baką). Program powinien czytać ze standardowego wejścia i pisać na standardowe wyjście. Przydatny może okazać się zestaw polskich liter diakrytyzowanych: 'ąćęłńóśźżĄĆĘŁŃÓŚŹŻ'.
 
+# cat pantadeusz.txt | python diac_sets.py
 
 # POINTS: 20
 
